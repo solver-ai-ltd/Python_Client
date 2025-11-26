8e900d7670aa9670c68c32b18dafe4cad870bcc8
tinifan0 <pfantini@solver-ai.com>
2025-11-26 13:42:07 +0000

    Updated client configuration

--- solverai/__init__.py
+++ solverai/__init__.py
@@ -1,4 +1,4 @@
-from .client_config import token, datamanagerUrl, computerUrl
+from .client_config import get_setup_data
 from .IdsFileManager import IdsFileManager
 from .SolverAiClientCompute import SolverAiClientCompute
 from .SolverAiClientExceptions import SolverAiClientExceptions
@@ -8,7 +8,7 @@ from .SolverAiComputeResults import SolverAiComputeResults
 from .SolverAiResultsWriter import SolverAiResultsWriter
 
 __all__ = [
-    "token", "datamanagerUrl", "computerUrl",
+    "get_setup_data",
     "IdsFileManager",
     "SolverAiClientCompute",
     "SolverAiClientExceptions",
--- solverai/client_config.py
+++ solverai/client_config.py
@@ -1,20 +1,25 @@
-from os import path
+def get_setup_data(setup_file_path):
+    with open(setup_file_path, 'r') as f:
+        lines = f.readlines()
 
+    config = {}
+    for line in lines:
+        key, value = line.strip().split('=')
+        config[key] = value
 
-data_file_folder_path = path.join(path.dirname(path.abspath(__file__)),
-                                  '..', 'example_files')
-setupFolder = path.join(path.dirname(path.abspath(__file__)),
-                        '..', 'setup')
-setupFilePath = path.join(setupFolder, 'setup.txt')
+    token = config.get('token', '')
+    datamanagerUrl = config.get('datamanagerUrl', '')
+    computerUrl = config.get('computerUrl', '')
 
-with open(setupFilePath, 'r') as f:
-    lines = f.readlines()
+    errors = []
+    if not token:
+        errors.append('"token" parameter not found in setup')
+    if not datamanagerUrl:
+        errors.append('"datamanagerUrl" parameter not found in setup')
+    if not computerUrl:
+        errors.append('"computerUrl" parameter not found in setup')
 
-config = {}
-for line in lines:
-    key, value = line.strip().split('=')
-    config[key] = value
+    if errors:
+        raise Exception('/n'.join(errors))
 
-token = config.get('token', '')
-datamanagerUrl = config.get('datamanagerUrl', '')
-computerUrl = config.get('computerUrl', '')
+    return token, datamanagerUrl, computerUrl
