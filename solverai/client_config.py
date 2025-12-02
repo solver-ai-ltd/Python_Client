69124e7e6337adf70385d88567f6ed76610705b4
tinifan0 <pfantini@solver-ai.com>
2025-12-02 15:54:21 +0000

    Added validate token function

--- solverai/client_config.py
+++ solverai/client_config.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def get_setup_data(setup_file_path):
     with open(setup_file_path, 'r') as f:
         lines = f.readlines()
@@ -26,3 +29,15 @@ def get_setup_data(setup_file_path):
         raise Exception('/n'.join(errors))
 
     return token, datamanagerUrl, computerUrl
+
+
+def validate_token(datamanagerUrl, token):
+    url = f'{datamanagerUrl}/api/data/validate-token/'
+    try:
+        headers = {'Authorization': f'Token {token}'}
+        response = requests.get(url, headers=headers, timeout=60)
+        if response.status_code == 200:
+            return True
+        return False
+    except Exception:
+        raise Exception('Failed verifying token.')
